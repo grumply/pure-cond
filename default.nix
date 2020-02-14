bebeb806a0aa53b80a1e8f4661dc3fa6e3f3b9fb
Sean Hickman <sean@grumply.com>
2020-02-13 23:29:59 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, containers, pure-core, pure-default, pure-json, pure-lifted, pure-try, pure-txt, unordered-containers, vector, stdenv }:
 mkDerivation {
   pname = "pure-cond";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base containers pure-core pure-default pure-json pure-lifted pure-try pure-txt unordered-containers vector ];
   homepage = "github.com/grumply/pure-cond";
